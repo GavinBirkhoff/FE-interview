9cd6669577126d76b57ca15c7e99423d21d4a049
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-08-29 20:28:04 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -25,6 +25,26 @@ module.exports = {
     sidebar: [
       "/",
       {
+        title: "Angular",
+        path: "/angular/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("angular"),
+        priority:10,
+      },
+      {
+        title: "Browser",
+        path: "/browser/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("browser"),
+        priority:10,
+      },
+      {
         title: "Css",
         path: "/css/",
         collapsable: true,
@@ -32,6 +52,67 @@ module.exports = {
         collapsable: false,
         // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
         children: getChildrenPaths("css"),
+        priority:10,
+      },
+      {
+        title: "Devops",
+        path: "/devops/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("devops"),
+        priority:10,
+      },
+      {
+        title: "Docker",
+        path: "/docker/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("docker"),
+        priority:10,
+      },
+      {
+        title: "Engineering",
+        path: "/engineering/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("engineering"),
+        priority:10,
+      },
+      {
+        title: "Git",
+        path: "/git/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("git"),
+        priority:10,
+      },
+      {
+        title: "Html",
+        path: "/html/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("html"),
+        priority:10,
+      },
+      {
+        title: "Http",
+        path: "/http/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("http"),
+        priority:10,
       },
       {
         title: "Javascript",
@@ -41,33 +122,57 @@ module.exports = {
         collapsable: false,
         // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
         children: getChildrenPaths("javascript"),
+        priority:10,
       },
       {
-        title: "Typescript",
-        path: "/typescript/",
+        title: "Jest",
+        path: "/jest/",
         collapsable: true,
         sidebarDepth: 1,
         collapsable: false,
         // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
-        children: getChildrenPaths("typescript"),
+        children: getChildrenPaths("jest"),
+        priority:10,
       },
       {
-        title: "React",
-        path: "/react/",
+        title: "linux",
+        path: "/linux/",
         collapsable: true,
         sidebarDepth: 1,
         collapsable: false,
         // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
-        children: getChildrenPaths("react"),
+        children: getChildrenPaths("linux"),
+        priority:10,
       },
       {
-        title: "Vue",
-        path: "/vue/",
+        title: "MobileWeb",
+        path: "/mobileWeb/",
         collapsable: true,
         sidebarDepth: 1,
         collapsable: false,
         // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
-        children: getChildrenPaths("vue"),
+        children: getChildrenPaths("mobileWeb"),
+        priority:10,
+      },
+      {
+        title: "Nextjs",
+        path: "/nextjs/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("nextjs"),
+        priority:10,
+      },
+      {
+        title: "Nginx",
+        path: "/nginx/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("nginx"),
+        priority:10,
       },
       {
         title: "Nodejs",
@@ -77,17 +182,79 @@ module.exports = {
         collapsable: false,
         // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
         children: getChildrenPaths("nodejs"),
+        priority:10,
       },
       {
-        title: "HTTPS?/计算机网络",
-        path: "/http/",
+        title: "Npm",
+        path: "/npm/",
         collapsable: true,
         sidebarDepth: 1,
         collapsable: false,
         // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
-        children: getChildrenPaths("http"),
+        children: getChildrenPaths("npm"),
+        priority:10,
       },
-    ],
+      {
+        title: "React",
+        path: "/react/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("react"),
+        priority:10,
+      },
+      {
+        title: "Rollupjs",
+        path: "/rollupjs/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("rollupjs"),
+        priority:10,
+      },
+      {
+        title: "Shell",
+        path: "/shell/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("shell"),
+        priority:10,
+      },
+      {
+        title: "Typescript",
+        path: "/typescript/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("typescript"),
+        priority:10,
+      },
+      {
+        title: "Vue",
+        path: "/vue/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("vue"),
+        priority:10,
+      },
+      {
+        title: "Webpack",
+        path: "/webpack/",
+        collapsable: true,
+        sidebarDepth: 1,
+        collapsable: false,
+        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
+        children: getChildrenPaths("webpack"),
+        priority:10,
+      },
+    ].sort((a,b)=>a.priority - b.priority),
     //   sidebar: 'auto'
   },
 };
