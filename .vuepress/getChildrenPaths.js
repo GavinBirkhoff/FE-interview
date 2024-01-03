76a1d5b62ae6d166e24ad4dcaee599a315fd8df5
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2024-01-03 21:17:20 +0800

    fix md path

--- .vuepress/getChildrenPaths.js
+++ .vuepress/getChildrenPaths.js
@@ -1,14 +1,17 @@
 const path = require("path");
-const fs = require('fs')
+const fs = require("fs");
 
 const cwd = process.cwd();
 const getChildrenPaths = (dir) => {
   const p = path.join(cwd, dir);
-    const files = fs.readdirSync(p,(err, files)=>{
-        if(err) throw err
-        return files
-    }).map(item=>path.join('/',dir,item).replace('.md','')).filter(item=>!item.includes('README'))
-  return files.filter(file=>file!=='/javascript/img');
+  const files = fs
+    .readdirSync(p, (err, files) => {
+      if (err) throw err;
+      return files;
+    })
+    .map((item) => path.join("/", dir, item).replace(".md", ""))
+    .filter((item) => !item.includes("README"));
+  return files.filter((file) => !file.includes('/img'));
 };
 
 module.exports = getChildrenPaths;
